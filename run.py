efc66aa92c8c6b11408724bbd13a41f46f95eb35
danjovg <josuevg4810@gmail.com>
2025-05-11 18:56:26 -0600

    Upgrade

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
 
-app = create_app()
+app = create_app()  # 👈 Define globalmente sin condición
 
+# El bloque siguiente es solo para desarrollo local
 if __name__ == '__main__':
     app.run(debug=True)
